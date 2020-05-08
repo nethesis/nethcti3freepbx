2cace7ec134f834f7ab7bedcc01f1e2fe862331e
Stefano Fancello <stefano.fancello@nethesis.it>
2020-05-08 18:27:12 +0200

    Take notification server credentails from template

--- agi-bin/app_wakeup.php
+++ agi-bin/app_wakeup.php
@@ -76,12 +76,7 @@ if (empty($results)) {
 }
 
 // Get credentials for notification server
-exec("/usr/bin/sudo /sbin/e-smith/config getprop subscription SystemId", $tmp);
-$lk = $tmp[0];
-unset($tmp);
-exec("/usr/bin/sudo /sbin/e-smith/config getprop subscription Secret", $tmp);
-$secret = $tmp[0];
-unset($tmp);
+$serverCredentials = json_decode(file_get_contents('/etc/asterisk/nethcti_push_configuration.json'));
 
 // Wake up extensions
 $extensions_to_wake = array();
@@ -95,7 +90,7 @@ foreach ($results as $result) {
     $data = array(
         "Message" => "",
         "TypeMessage" => 2,
-        "UserName" => $username.'@'.gethostname(),
+        "UserName" => $username.'@'.$serverCredentials['Host'],
         "Sound" => "",
         "Badge" => 0,
         "CustomField1" => "IC_MSG",
@@ -106,7 +101,7 @@ foreach ($results as $result) {
     curl_setopt($ch, CURLOPT_URL, "https://pp.nethesis.it/NotificaPush");
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_USERPWD, $lk . ':' . $secret);
+    curl_setopt($ch, CURLOPT_USERPWD, $serverCredentials['SystemId'] . ':' . $serverCredentials['Secret']);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         "Content-Type: application/json",
         "X-HTTP-Method-Override: SendPushAuth",
